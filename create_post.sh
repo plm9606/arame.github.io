8934de5cbbd3c1c456ba7a760b5c562553ed1892
Adam <adam@daangn.com>
2024-09-21 15:03:11 +0900

    fix: post cli

--- create_post.sh
+++ create_post.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
-# 현재 날짜를 가져옵니다.
-current_date=$(date +"%Y-%m-%d %H:%M:%S")
+# title을 입력받습니다.
+echo "Enter the title: "
+read title
 
-# 파일 이름을 생성합니다.
-filename="${current_date:0:10}.md"
+
+
+# 현재 날짜를 가져옵니다.
+current_date=$(date +"%Y-%m-%d")
+
+# 파일 이름을 생성합니다. (yyyy-mm-dd-title.md)
+# filename=$(echo $current_date | sed 's/ /-/' | sed 's/ /-/g' | sed 's/:/-/g')-$(echo $title | tr '[:upper:]' '[:lower:]' | tr -d '[:punct:]' | tr ' ' '-').md
+filename=$(echo $current_date)-$(echo $title | tr '[:upper:]' '[:lower:]' | tr -d '[:punct:]' | tr -d '[:space:]').md
 
 # 파일 경로를 설정합니다.
 file_path="_posts/${filename}"
@@ -14,8 +21,7 @@ touch $file_path
 
 # 파일에 내용을 추가합니다.
 echo "---" >> $file_path
-echo "title: " >> $file_path
-echo "date: ${current_date}" >> $file_path
+echo "title: ${title}" >> $file_path
 echo "categories: []" >> $file_path
 echo "tags: []" >> $file_path
 echo "author: aram" >> $file_path
